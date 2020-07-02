11318d2f2ffe52224031ae43fdc1cb80120c41c5
Gabriel Heberle <gheberledude@gmail.com>
2020-07-02 19:17:44 -0300

    Alterações em variáveis de main.c

--- main_TEST_CASE.cpp
+++
@@ -1,9 +0,0 @@
-#define CATCH_CONFIG_MAIN
-
-#include "catch.hpp"
-
-TEST_CASE( "1: Inicializando sequência de testes para programa IRPF", "[multi-file:1]" )
-{}
-
-#include "calcSimplificado_TEST_CASE.cpp"
-#include "calcCompleto_TEST_CASE.cpp"
